b49d35f9d3823aaf8ea0ac3e49f5babd2a960d98
Thogg <garnier.theo92@gmail.com>
2023-07-10 16:32:27 +0300

    Commentary and add zcr

--- Representation/GlobalRepresentation/App.py
+++ Representation/GlobalRepresentation/App.py
@@ -6,46 +6,63 @@ from classRepresentation import *
 
 
 def newwindow():
-    root = tk.Tk()
-    mainframe = tk.Frame(root, background="Blue")
+    # Global varibles for the intern function
+    global audio
+    global sr
 
+    # Create the window
+    root = tk.Tk()
+
+    # Create the frame with the graphs
+    mainframe = tk.Frame(root)
+
+    # Open the os file browser and change the audio studied accordingly
     def browseFiles():
-        global audio
-        global sr
         filename = tk.filedialog.askopenfilename(
             initialdir = "Representation\BeeDataset\\",
             title = "Select a File",
             filetypes = (("wav files", "*.wav"), ("None", ""))
         )
 
+        # Check if "Cancel" wasn't clicked
         if filename != "":
             audio, sr = getaudiofromfile(filename)
 
+            # Change the audio studied and goes back to FFT
             fft.changeto(audio, sr, fig, canva)
             filename = filename.split('/', 100)[-1] # Only get the file name and no the path
             root.title(filename)
 
+    # Add a menu bar up right
     menubar = tk.Menu(root)
     filemenu = tk.Menu(menubar, tearoff=0)
+
+    # menu to open a new file
     filemenu.add_command(label="Open audio file...", command=browseFiles, accelerator="Ctrl+O")
     root.bind_all("<Control-o>", lambda event: browseFiles())
 
+    # menu to open a window with every mode shown
     filemenu.add_command(label="Detailed window", command=showAllRepresentation)
 
+    # menu to open a new intialized window
     filemenu.add_command(label="New window", command=newwindow, accelerator="Ctrl+N")
     root.bind_all("<Control-n>", lambda event: newwindow())
 
+    # menu to quit the app
     filemenu.add_command(label="Quit", command=root.destroy, accelerator="Ctrl+W")
     root.bind_all("<Control-w>", lambda event: root.destroy())
     
+    # Add the file sub category with all the button above
     menubar.add_cascade(label="File", menu=filemenu)
     root.config(menu=menubar)
 
+    # Configure the plt to go on the frame
     fig = plt.Figure()
     ax = fig.add_subplot(111)
     canva = FigureCanvasTkAgg(fig, mainframe)
     canva.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
 
+    # Call all the different modes
     wave = Waveclass()
     fft = FFTclass()
     chromafeature = ChromaFeatureclass()
@@ -53,6 +70,7 @@ def newwindow():
     mfcc = MFCCclass()
     mfccdelta = MFCCdeltaclass()
     spectralcentroid = spectralcentroidclass()
+    zcr = ZCRClass()
 
     # First example on startup
     FILE_NAME = 'Hive3_20_07_2017_QueenBee_H3_audio___06_20_00_21.wav'
@@ -61,44 +79,29 @@ def newwindow():
     audio, sr = getaudiofromfile(PATH_NAME)
     wave.plotfct(audio, sr, ax)
 
-
+    # Pack the frame with the graph
     mainframe.pack()
 
     # Frame below with the buttons
     changemodeframe = tk.Frame(root, bg="white")
 
+    # generic function to create buttons
     def create_button(mode, row, column):
         button = tk.Button(changemodeframe, text=mode.text, command=lambda: mode.changeto(audio, sr, fig, canva))
         button.grid(row=row, column=column)
 
-    existingmode = [wave, fft, chromafeature, spectrogram, mfcc, mfccdelta, spectralcentroid]
+    # mode with which we can study an audio file
+    existingmode = [wave, fft, chromafeature, spectrogram, mfcc, mfccdelta, spectralcentroid, zcr]
 
+    # Creating the buttons
     for index, mode in enumerate(existingmode):
         create_button(mode, row=1, column=index+1)
 
-    """button = tk.Button(changemodeframe, text="Wave", command=lambda : wave.changeto(audio, sr, fig, canva))
-    button.grid(row=1, column=1)
-
-    button = tk.Button(changemodeframe, text="FFT", command=lambda : fft.changeto(audio, sr, fig, canva))
-    button.grid(row=1, column=2)
-
-    button = tk.Button(changemodeframe, text="MFCC", command=lambda : mfcc.changeto(audio, sr, fig, canva))
-    button.grid(row=1, column=3)
-
-    button = tk.Button(changemodeframe, text="Spectrogram", command=lambda : spectrogram.changeto(audio, sr, fig, canva))
-    button.grid(row=1, column=4)
-
-    button = tk.Button(changemodeframe, text="Chroma feature", command=lambda : chromafeature.changeto(audio, sr, fig, canva))
-    button.grid(row=1, column=5)
-
-    button = tk.Button(changemodeframe, text="MFCCDelta", command=lambda : mfccdelta.changeto(audio, sr, fig, canva))
-    button.grid(row = 1, column=6)
-
-    button = tk.Button(changemodeframe, text="Spectral Centroid", command=lambda : spectralcentroid.changeto(audio, sr, fig, canva))
-    button.grid(row=1, column=7)"""
-
+    # Put everything together
     changemodeframe.pack()
     root.title(FILE_NAME)
+
+    # launch the app
     root.mainloop()
 
 if __name__ == "__main__":
