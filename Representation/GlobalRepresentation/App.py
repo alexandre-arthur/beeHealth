6a9bb4651b010f6b57f98fedbe68ce41d796aa72
Thogg <garnier.theo92@gmail.com>
2023-07-13 12:10:16 +0300

    Improve di app

--- Representation/GlobalRepresentation/App.py
+++ Representation/GlobalRepresentation/App.py
@@ -1,37 +1,63 @@
 import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from GlobalFiguresBees import * 
-from classRepresentation import * 
-
+from GlobalFiguresBees import *
+from classRepresentation import *
+from PIL import ImageTk, Image
+import playsound
 
 def newwindow():
     # Global varibles for the intern function
     global audio
     global sr
+    bgcolor = 'white'
+
+    # First example on startup
+    FILE_NAME = 'Hive3_20_07_2017_QueenBee_H3_audio___06_20_00_21.wav'
+    PATH_NAME = 'Representation\BeeDataset\OnlyBees\QueenBee\\' + FILE_NAME
+    #PATH_NAME = 'Representation\BeeDataset\OnlyBees\Ionian_mode_C.wav'
 
     # Create the window
     root = tk.Tk()
+    root.configure(background=bgcolor)
+    root.state('zoomed')
 
-    # Create the frame with the graphs
-    mainframe = tk.Frame(root)
+
+
+    # Create the title
+    def titleshow(text):
+        global titleframe
+        titleframe = tk.Frame(root)
+        titleframe.configure(background=bgcolor)
+        title = tk.Label(titleframe, text=text, font=("Arial", 15), bg="white")
+        title.pack()
+        titleframe.grid(row=1, column=1)
+
+    titleshow(FILE_NAME)
+
+
 
     # Open the os file browser and change the audio studied accordingly
     def browseFiles():
-        filename = tk.filedialog.askopenfilename(
+        global audio
+        global sr
+        global PATH_NAME
+        PATH_NAME = tk.filedialog.askopenfilename(
             initialdir = "Representation\BeeDataset\\",
             title = "Select a File",
             filetypes = (("wav files", "*.wav"), ("None", ""))
         )
 
         # Check if "Cancel" wasn't clicked
-        if filename != "":
+        if PATH_NAME != "":
             audio, sr = getaudiofromfile(filename)
 
             # Change the audio studied and goes back to FFT
             fft.changeto(audio, sr, fig, canva)
+            PATH_NAME = filename
             filename = filename.split('/', 100)[-1] # Only get the file name and no the path
-            root.title(filename)
+            titleframe.destroy()
+            titleshow(filename)
 
     # Add a menu bar up right
     menubar = tk.Menu(root)
@@ -56,11 +82,31 @@ def newwindow():
     menubar.add_cascade(label="File", menu=filemenu)
     root.config(menu=menubar)
 
+
+
+    # Create the frame with the graphs
+    mainframe = tk.Frame(root)
+    mainframe.configure(background=bgcolor)
+
     # Configure the plt to go on the frame
+    plotframe = tk.Frame(mainframe)
     fig = plt.Figure()
     ax = fig.add_subplot(111)
-    canva = FigureCanvasTkAgg(fig, mainframe)
+    canva = FigureCanvasTkAgg(fig, plotframe)
+    # Increase the width of the graph and the width of the frame
+    canva.get_tk_widget().configure(width=root.winfo_screenwidth()-20*7, height=500)
     canva.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+
+
+    #Get the current screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    #Print the screen size
+    print("Screen width:", screen_width)
+    print("Screen height:", screen_height)
+
+    plotframe.pack(side=tk.BOTTOM, fill=tk.BOTH)
 
     # Call all the different modes
     wave = Waveclass()
@@ -71,36 +117,61 @@ def newwindow():
     mfccdelta = MFCCdeltaclass()
     spectralcentroid = spectralcentroidclass()
     zcr = ZCRClass()
+    constantq = ConstantQclass()
+    variableq = VariableQclass()
+    energynormalized = EnergyNormalizedclass()
+    bandwidth = Bandwidthclass()
+    spectralcentroid = SpectralContrastclass()
+    spectralflatness = SpectralFlatnessclass()
+    spectralrolloff = SpectralRolloffclass()
+    tonalCentroid = TonalCentroidclass()
 
-    # First example on startup
-    FILE_NAME = 'Hive3_20_07_2017_QueenBee_H3_audio___06_20_00_21.wav'
-    PATH_NAME = 'Representation\BeeDataset\OnlyBees\QueenBee\\' + FILE_NAME
-    PATH_NAME = 'Representation\BeeDataset\OnlyBees\Ionian_mode_C.wav'
     audio, sr = getaudiofromfile(PATH_NAME)
     wave.plotfct(audio, sr, ax)
 
     # Pack the frame with the graph
-    mainframe.pack()
+    mainframe.grid(row=2, column=1)
+    mainframe.configure(background=bgcolor)
+
+
+
+    # Create the frame with an image
+    imageframe = tk.Frame(root)
+    imageframe.configure(background=bgcolor)
+    imagepath = "Representation\GlobalRepresentation\Cartoon-Bee.png"
+    img = Image.open(imagepath)
+    img = img.resize((140,110), Image.ANTIALIAS)
+    new_img = ImageTk.PhotoImage(img)
+    PATH_NAME = 'Representation\BeeDataset\OnlyBees\QueenBee\Hive3_20_07_2017_QueenBee_H3_audio___06_20_00_21.wav'
+    #sound = playsound.playsound(PATH_NAME, block=False)
+    #image = tk.Button(imageframe, image = new_img, background=bgcolor, width=140, height=50, borderwidth=0, relief="sunken", command=sound)
+    #image.pack()
+    imageframe.grid(row=1, column=2)
+
+
 
     # Frame below with the buttons
     changemodeframe = tk.Frame(root, bg="white")
 
     # generic function to create buttons
     def create_button(mode, row, column):
-        button = tk.Button(changemodeframe, text=mode.text, command=lambda: mode.changeto(audio, sr, fig, canva))
+        button = tk.Button(changemodeframe, text=mode.text, width=20, height=2, command=lambda: mode.changeto(audio, sr, fig, canva))
         button.grid(row=row, column=column)
 
     # mode with which we can study an audio file
-    existingmode = [wave, fft, chromafeature, spectrogram, mfcc, mfccdelta, spectralcentroid, zcr]
+    existingmode = [wave, fft, chromafeature, spectrogram, mfcc, mfccdelta, spectralcentroid, zcr, constantq, variableq, energynormalized, bandwidth, spectralflatness, spectralrolloff, tonalCentroid]
 
     # Creating the buttons
     for index, mode in enumerate(existingmode):
-        create_button(mode, row=1, column=index+1)
+        create_button(mode, row=index+1, column=1)
 
     # Put everything together
-    changemodeframe.pack()
-    root.title(FILE_NAME)
+    changemodeframe.grid(row=2, column=2)
 
+
+
+    root.title(FILE_NAME)
+    root.title("Beehealth - Representation")
     # launch the app
     root.mainloop()
 
