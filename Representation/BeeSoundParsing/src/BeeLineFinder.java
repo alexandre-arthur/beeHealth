52e96a8f9285337e5e6eb1343e31bf939043cbd7
Thogg <garnier.theo92@gmail.com>
2023-07-18 15:44:18 +0300

    Improve Bee line finders (added NObee)

--- Representation/BeeSoundParsing/src/BeeLineFinder.java
+++ Representation/BeeSoundParsing/src/BeeLineFinder.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class BeeLineFinder {
     public static void main(String[] args) {
-        File folder = new File("BeeDataset/FromWeb");
+        File folder = new File("Representation/BeeDataset/FromWeb");
         File[] listOfFiles = folder.listFiles();
 
         int sizeOfExtension = 4;
@@ -25,41 +25,60 @@ public class BeeLineFinder {
     }
 
     public static void parseAFile(String fileName) {
-        String genericFilePath = "BeeDataset/FromWeb/" + fileName;
+        // Reader path
+        String genericFilePath = "Representation/BeeDataset/FromWeb/" + fileName;
         String dataFilePath = genericFilePath + ".lab"; // Update with the actual file path
         String audioFilePath = genericFilePath + ".wav";
 
+        // init Variables
         int lineWithBees = 0;
-        ArrayList<Double> beginTimeStamps = new ArrayList<>();
-        ArrayList<Double> endTimeStamps = new ArrayList<>();
+        ArrayList<Integer> beginTimeStamps = new ArrayList<>();
+        ArrayList<Integer> endTimeStamps = new ArrayList<>();
+        ArrayList<String> beeLines = new ArrayList<>();
         // Read the lab file
         try (BufferedReader reader = new BufferedReader(new FileReader(dataFilePath))) {
             String line;
 
+            // read the lines one by one and get the time stamps
             for(int lineNumber = 1; (line = reader.readLine()) != null; lineNumber++) {
                 if (line.contains("bee") && !line.contains("nobee")) {
-                    String[] parts = line.split("\t");
-                    String number1Str = parts[0].replace(",", ".");
-                    String number2Str = parts[1].replace(",", ".");
-                    double number1 = Double.parseDouble(number1Str);
-                    double number2 = Double.parseDouble(number2Str);
-                    beginTimeStamps.add(number1);
-                    endTimeStamps.add(number2);
-                    lineWithBees++;
-                    System.out.println("Line " + lineNumber + ": Number1=" + number1 + ", Number2=" + number2);
+                    beeLines.add("B");
                 }
+                else if(line.contains("nobee")) {
+                    beeLines.add("N");
+                }
+                else { 
+                    continue;
+                }
+                System.out.println(dataFilePath);
+                String[] parts = line.split("\t");
+                String number1Str = parts[0].replace(",", ".");
+                String number2Str = parts[1].replace(",", ".");
+                double number1 = Double.parseDouble(number1Str);
+                double number2 = Double.parseDouble(number2Str);
+                int number1Int = (int) Math.ceil(number1);
+                int number2Int = (int) Math.ceil(number2);
+                beginTimeStamps.add(number1Int);
+                endTimeStamps.add(number2Int);
+                lineWithBees++;
+                System.out.println("Line " + lineNumber + ": Number1=" + number1Int + ", Number2=" + number2Int);
             }
         } catch (IOException e) {
             System.err.println("Error reading the file: " + e.getMessage());
         }
 
         // Write the audio file sliced
-        String pathName = "BeeDataset/OnlyBees/";
+        String pathName = "Representation/BeeDataset/TheBeetles/";
         for(int i = 0; i < lineWithBees; i++) {
+            int begin = beginTimeStamps.get(i);
+            int end = endTimeStamps.get(i);
+            String name = beeLines.get(i);
+            if(end != begin) {
             AudioFileProcessor.copyAudio(audioFilePath,
-                    pathName + fileName + "_" + i + ".wav",
-                    beginTimeStamps.get(i).intValue(),
-                    endTimeStamps.get(i).intValue() - beginTimeStamps.get(i).intValue());
+                    pathName + fileName + "_" + i + "_" + name + ".wav",
+                    begin,
+                    end - begin);
+            }
         }
     }
 }
