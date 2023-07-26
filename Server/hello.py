8d505c4b566fe2950a1a0b6435afac263625dec0
Alexandre <alexandre.arthur@telecom-paris.fr>
2023-07-26 16:49:24 +0200

    Add the website to the webserver

--- Server/hello.py
+++ Server/hello.py
@@ -2,10 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+@app.route('/beeHealth_main.html')
+def beeHealth_main():
+    return render_template('beeHealth_main.html')
+
+@app.route('/beeHealth_data.html')   
+def beeHealth_data():
+    return render_template('beeHealth_data.html')
+
+#generate autamatically the route for each analysis html file !
+@app.route('/13-12-25-07-2023.html')
+def beeHealth_13_12_25_07_2023():
+    return render_template('13-12-25-07-2023.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
