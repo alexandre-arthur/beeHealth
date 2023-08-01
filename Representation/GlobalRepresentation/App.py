0debc028ad9f1207968e7cd85bf7ac7ae55dbcc0
Thogg <garnier.theo92@gmail.com>
2023-08-01 23:22:52 +0300

    debut rework App.py

--- Representation/GlobalRepresentation/App.py
+++ Representation/GlobalRepresentation/App.py
@@ -7,41 +7,51 @@ from PIL import ImageTk, Image
 import winsound
 
 class ToolTip:
+    # Create a tooltip when the mouse hover on a button
     def __init__(self,widget,text=None):
+    # Create a tooltip when the mouse hover on a button
 
         def on_enter(event):
+            # Function when the mouse enter the button
+
+            # Create the tooltip
             self.tooltip=tk.Toplevel()
             self.tooltip.overrideredirect(True)
-            width = 150
-            print(str(self.widget.winfo_rooty()) + "    " + str(event.y_root))
+
+            # Place the tooltip
             self.tooltip.geometry(f'+{event.x_root-900}+{self.widget.winfo_rooty()+10}')
             self.tooltip.background='white'
 
+            # Create the text in the tooltip
             self.label=tk.Label(self.tooltip,text=self.text, height=2, width=100)
             self.label.config(font=("Arial", 10))
             self.label.configure(wraplength=800, justify="left", background='white', borderwidth=1, relief="solid")
             self.label.pack(side='right')
 
         def on_leave(event):
+            # Function when the mouse leave the button
             self.tooltip.destroy()
 
+        # Global variables
         self.widget=widget
         self.text=text
 
+        # Bind the function to the button
         self.widget.bind('<Enter>',on_enter)
         self.widget.bind('<Leave>',on_leave)
 
+
 def newwindow():
     # Global varibles for the intern function
-    global audio
-    global sr
-    global bgcolor
+    #global audio
+    #global sr
+    #global bgcolor
     # set the background color to light grey
     bgcolor = "#D3D3D3"
 
     # First example on startup
     FILE_NAME = 'Hive3_20_07_2017_QueenBee_H3_audio___06_20_00_21.wav'
-    PATH_NAME = 'Representation\BeeDataset\OnlyBees\QueenBee\\' + FILE_NAME
+    PATH_NAME = 'Audio\BeeDataset\OnlyBees\QueenBee\\' + FILE_NAME
     #PATH_NAME = 'Representation\BeeDataset\OnlyBees\Ionian_mode_C.wav'
 
     # Create the window
@@ -49,43 +59,36 @@ def newwindow():
     root.configure(background=bgcolor)
     root.state('zoomed')
 
-
-
     # Create the title
-    def titleshow(text):
-        global bgcolor
-        global titleframe
-        titleframe = tk.Frame(root)
-        titleframe.configure(background=bgcolor)
-        title = tk.Label(titleframe, text=text, font=("Arial", 15), background=bgcolor, fg="black")
-        title.pack()
-        titleframe.grid(row=1, column=1)
 
-    titleshow(FILE_NAME)
-
-
+    titleFrame = changeTitleFrame(root, FILE_NAME, bgcolor)
+    titleFrame.grid(row=1, column=1)
 
     # Open the os file browser and change the audio studied accordingly
     def browseFiles():
         global audio
         global sr
         global PATH_NAME
-        PATH_NAME = tk.filedialog.askopenfilename(
-            initialdir = "Representation\BeeDataset\\",
+        newSoundFile = tk.filedialog.askopenfilename(
+            initialdir = "Audio\BeeDataset\\",
             title = "Select a File",
             filetypes = (("wav files", "*.wav"), ("None", ""))
         )
 
         # Check if "Cancel" wasn't clicked
-        if PATH_NAME != "":
-            audio, sr = getaudiofromfile(PATH_NAME)
+        if newSoundFile != "":
+            audio, sr = getaudiofromfile(newSoundFile)
+            PATH_NAME = newSoundFile
 
             # Change the audio studied and goes back to FFT
             fft.changeto(audio, sr, fig, canva)
-            PATH_NAME = filename
+
+            # Change the title
+            filename = newSoundFile
             filename = filename.split('/', 100)[-1] # Only get the file name and no the path
-            titleframe.destroy()
-            titleshow(filename)
+            titleFrame.destroy()
+            changeTitleFrame(root, filename, bgcolor)
+            titleFrame.grid(row=1, column=1)
 
     # Add a menu bar up right
     menubar = tk.Menu(root)
@@ -110,32 +113,9 @@ def newwindow():
     menubar.add_cascade(label="File", menu=filemenu)
     root.config(menu=menubar)
 
-
-
-    # Create the frame with the graphs
-    mainframe = tk.Frame(root)
-    mainframe.configure(background=bgcolor)
-
-    # Configure the plt to go on the frame
-    plotframe = tk.Frame(mainframe, bg=bgcolor)
-    fig = plt.Figure()
-    fig.set_facecolor(bgcolor)
-    ax = fig.add_subplot(111)
-    canva = FigureCanvasTkAgg(fig, plotframe)
-    # Increase the width of the graph and the width of the frame
-    canva.get_tk_widget().configure(width=root.winfo_screenwidth()-20*7, height=500, background=bgcolor)
-    canva.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
-
-
-    #Get the current screen width and height
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    #Print the screen size
-    print("Screen width:", screen_width)
-    print("Screen height:", screen_height)
-
-    plotframe.pack(side=tk.BOTTOM, fill=tk.BOTH)
+    mainFrame, fig, canva = createGraphFrame(root, PATH_NAME, bgColor=bgcolor)
+    
+    mainFrame.grid(row=2, column=1)
 
     # Call all the different modes
     wave = Waveclass()
@@ -154,15 +134,6 @@ def newwindow():
     spectralflatness = SpectralFlatnessclass()
     spectralrolloff = SpectralRolloffclass()
     tonalCentroid = TonalCentroidclass()
-
-    audio, sr = getaudiofromfile(PATH_NAME)
-    wave.plotfct(audio, sr, ax)
-
-    # Pack the frame with the graph
-    mainframe.grid(row=2, column=1)
-    mainframe.configure(background=bgcolor)
-
-
 
     # Create the frame with an image
     imageframe = tk.Frame(root)
@@ -183,7 +154,7 @@ def newwindow():
 
     # generic function to create buttons
     def create_button(mode, row, column):
-        button = tk.Button(changemodeframe, text=mode.text, width=20, height=2, command=lambda: mode.changeto(audio, sr, fig, canva))
+        button = tk.Button(changemodeframe, text=mode.getTitle(), width=20, height=2, command=lambda: mode.changeto(audio, sr, fig, canva))
         button.grid(row=row, column=column)
         ToolTip(button, mode.description)
 
@@ -203,6 +174,63 @@ def newwindow():
     root.title("Beehealth - Representation")
     # launch the app
     root.mainloop()
+
+
+
+
+def changeTitleFrame(root, text : str, bgColor : str = "#FFFFFF", fgColor : str = "#000000", font : (str, int) = ("Arial", 15)):
+    # Create a new frame to display the title
+    # @arguments : - root : the root of the window
+    #              - text : The title to display
+    #              - bgColor : The background color of the frame (default : black)
+    #              - fgColor : The foreground color of the frame (default : white)
+    # @return : - titleFrame : The frame with the title on it
+
+    # Create a new frame
+    titleFrame = tk.Frame(root)
+    titleFrame.configure(background=bgColor)
+
+    # Add the title on it
+    title = tk.Label(titleFrame, text=text, font=font, background=bgColor, fg=fgColor)
+    title.pack()
+    
+    return titleFrame
+
+def createGraphFrame(root, bgColor : str = "#FFFFFF"):
+    # Create the frame with the graphs
+    mainFrame = tk.Frame(root)
+    mainFrame.configure(background=bgColor)
+
+    # Inside frame to get the plot
+    plotframe = tk.Frame(mainFrame, bg=bgColor)
+
+    # Configure the plt to go on the frame
+    fig, canva = addPlotToFrame(plotframe, PATH_NAME, bgColor, width=root.winfo_screenwidth()-20*7)
+
+    plotframe.pack(side=tk.BOTTOM, fill=tk.BOTH)
+
+    # Pack the frame with the graph
+    mainFrame.configure(background=bgColor)
+
+    return mainFrame, fig, canva
+
+def addPlotToFrame(frame, pathName : str, bgColor : str = "#FFFFFF", width : int = 1000, height : int = 500):
+    # Configure the plt to go on the frame
+    fig = plt.Figure()
+    fig.set_facecolor(bgColor)
+    ax = fig.add_subplot(111)
+    canva = FigureCanvasTkAgg(fig, frame)
+
+    # Increase the width of the graph
+    canva.get_tk_widget().configure(width=width, height=height, background=bgColor)
+    canva.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+
+    # Put the default graph
+    audio, sampleRate = getaudiofromfile(pathName)
+    wave = Waveclass()
+    wave.plotfct(audio, sampleRate, ax)
+
+    return fig, canva
 
 if __name__ == "__main__":
     newwindow()
